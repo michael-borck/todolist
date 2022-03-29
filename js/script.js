b294caca3c5eafe4528c9725734e9a9e4260d6d8
michael-borck <91189447+michael-borck@users.noreply.github.com>
2022-03-29 12:45:47 +0800

    Added add task behaviour

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// This is empty
+// Step 1: Find element
+let addTaskButton = document.getElementById("add-task")
+let newTaskInput = document.getElementById("task-input")
+
+
+// Step 2: Write a function to implement the behaviour
+function onAddTaskClicked(event) {
+    let taskName = newTaskInput.value;
+    newTaskInput.value = "";
+    console.log(taskName)
+}
+
+// Step 3: Link element, function and event
+addTaskButton.addEventListener('click',onAddTaskClicked)
