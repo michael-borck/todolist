82b6263aa3107f5328006489dca596020857a830
michael-borck <91189447+michael-borck@users.noreply.github.com>
2022-03-29 13:17:43 +0800

    Append text to todo list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Step 1: Find element
 let addTaskButton = document.getElementById("add-task")
 let newTaskInput = document.getElementById("task-input")
+let todoListContainer =  document.getElementById("todo-list")
 
 
 // Step 2: Write a function to implement the behaviour
@@ -8,6 +9,7 @@ function onAddTaskClicked(event) {
     let taskName = newTaskInput.value;
     newTaskInput.value = "";
     console.log(taskName)
+    todoListContainer.insertAdjacentHTML("afterbegin",taskName);
 }
 
 // Step 3: Link element, function and event
